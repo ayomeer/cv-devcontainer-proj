9241f96fb6d339919b1c339ccf4e1fd573644ef9
DESKTOP-0SRFR83\ayome <andreas.kuhn@bluewin.ch>
2023-03-20 18:58:12 +0100

    Passing image from and back to python; Fixed order of pip and opencv install in Dockerfile so importing cv2 works again

--- cpp/cppmodule.cpp
+++ cpp/cppmodule.cpp
@@ -5,31 +5,35 @@
 
 namespace py = pybind11;
 
-cv::Mat readImage(){
-    cv::Mat image;
-    image = cv::imread("/app/_img/dog.jpg");
+cv::Mat pointwiseUndistort(py::array_t<unsigned char>& imBuf){
+    auto rows = imBuf.shape(0);
+    auto cols = imBuf.shape(1);
+    auto type = CV_8UC3; // 3 dim unsigned char
+    auto ptr = (unsigned char*)imBuf.data();
 
-    return image;
+    cv::Mat img(rows, cols, type, ptr);
+
+    cv::flip(img, img, 0);
+    return img;
 }
 
 PYBIND11_MODULE(cppmodule, m){
-    m.def("readImage", &readImage);
-    m.doc() = "Docstring for readImage function";
-
+    m.def("pointwiseUndistort", &pointwiseUndistort, py::return_value_policy::automatic);
+    m.doc() = "Docstring for pointwiseUndistort function";
     py::class_<cv::Mat>(m, "Mat", py::buffer_protocol())
         .def_buffer([](cv::Mat &im) -> py::buffer_info {
             return py::buffer_info(
                 im.data,                                            // pointer to data
                 sizeof(unsigned char),                              // item size
                 py::format_descriptor<unsigned char>::format(),     // item descriptor
-                3,                                            // matrix dimensionality
+                3,                                                  // matrix dimensionality
                 {                                                   // buffer dimensions
                     im.rows, 
                     im.cols, 
                     im.channels()
                 },          
                 {                                                   // strides in bytes
-                    sizeof(unsigned char) * im.channels() * im.cols, 
+                    sizeof(unsigned char) * im.channels() * im.cols, // (issue with padding)
                     sizeof(unsigned char) * im.channels(),
                     sizeof(unsigned char)
                 }
