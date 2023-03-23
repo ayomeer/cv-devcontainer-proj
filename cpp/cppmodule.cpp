243db421c88c281a0cfe39f737591000667b6f0f
DESKTOP-0SRFR83\ayome <andreas.kuhn@bluewin.ch>
2023-03-23 23:37:11 +0100

    [skip ci] Working (naive) undistort C++ implementation callable from python

--- cpp/cppmodule.cpp
+++ cpp/cppmodule.cpp
@@ -3,87 +3,88 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 
+
 namespace py = pybind11;
 
-/*
-void pointwiseUndistort(py::array& arr){
-    auto rows = arr.shape(0);
-    auto cols = arr.shape(1);
-    auto type = CV_8UC3; // 3 dim unsigned char
-    auto ptr = (unsigned char*)arr.data();
+typedef std::uint8_t imgScalar;
+typedef double matScalar;
 
-    cv::Mat img(rows, cols, type, ptr);
-    cv::flip(img, img, 1);
-}
-*/
+namespace cv {
+    Mat pointwiseUndistort( py::array_t<imgScalar>& pyImg_d, 
+                            py::array_t<matScalar>& pyH, 
+                            py::tuple img_u_shape ){
 
-/*
-void pointwiseUndistort(cv::Mat& arr){
+        // -- Data type management
+        // link pyImg_d data to cv::Mat object img
+        Mat img_d(
+            pyImg_d.shape(0),               // rows
+            pyImg_d.shape(1),               // cols
+            CV_8UC3,                        // data type
+            (imgScalar*)pyImg_d.data());    // data pointer
 
-    cv::flip(arr, arr, 1);
+        // link H data to cv::Mat object
+        Mat H(
+            pyH.shape(0),                   // rows
+            pyH.shape(1),                   // cols
+            CV_64FC1,                       // data type
+            (matScalar*)pyH.data());        // data pointer
 
-}
-*/
-
-
-cv::Mat pointwiseUndistort(cv::Mat& arr){
-
-    cv::flip(arr, arr, 1);
-    return arr;
-}
-
-
-
-PYBIND11_MODULE(cppmodule, m){
-    m.def("pointwiseUndistort", &pointwiseUndistort, py::return_value_policy::automatic);
-    m.doc() = "Docstring for pointwiseUndistort function";
+        int M = img_u_shape[0].cast<int>();
+        int N = img_u_shape[1].cast<int>();
     
-    py::class_<cv::Mat>(m, "Mat", py::buffer_protocol())      
-        // custom contructor for obj creation from numpy buffer object
-        .def(py::init([](py::buffer b) {
-            py::buffer_info info = b.request();
-            
-            auto dims = info.ndim;
-            auto rows = info.shape[0];
-            auto cols = info.shape[1];
-            
-            // determine cv::mat type 
-            int type;
-            if(dims == 2) {// transform matrix
-                type = CV_64FC2;
-                printf("\n Creating 2dim cv::Mat \n");
+        // -- Algorithm
+        Mat img_u(M, N, CV_8UC3); // prepare return image
+
+        for (int m=0; m<M; m++){ // m = {0, 1, 2, ... , M-1}
+            for (int n=0; n<N; n++){ 
+                Mat xu = (Mat_<int>(3, 1) << m, n, 1);
+                Mat xu_double;
+                xu.convertTo(xu_double, CV_64F);
+
+                // coordinate transform
+                Mat xd_double = H*xu_double;
+                
+                // convert back to inhom coords
+                xd_double = xd_double / xd_double.at<double>(2,0);
+
+                // round to integer indexes
+                Mat xd_int;
+                xd_double.convertTo(xd_int, CV_32S);
+
+                // use transformed coords to get pixel value from distorted image
+                int x = xd_int.at<int>(0,0); 
+                int y = xd_int.at<int>(1,0); 
+
+                Vec3b rgb_vect = img_d.at<Vec3b>(Point(y, x)); //reverso!
+                img_u.at<Vec3b>(m, n) = rgb_vect;
             }
-            else if (dims == 3){ // image
-                type = CV_8UC3;     
-                printf("\n Creating 3dim cv::Mat \n");
-            }
+        }
+        return img_u;
+    }       
 
-            return cv::Mat(rows, cols, type, info.ptr);
-        }))
+    PYBIND11_MODULE(cppmodule, m){
+        m.def("pointwiseUndistort", &pointwiseUndistort, py::return_value_policy::automatic);
+        m.doc() = "Docstring for pointwiseUndistort function";
 
-        // expose some public attributes
-        .def_readwrite("dims", &cv::Mat::dims)
-        .def_readwrite("data", &cv::Mat::data)
-
-        // define buffer object to hand over to Python C-API instead of passing by value
-        .def_buffer([](cv::Mat &im) -> py::buffer_info {
-            return py::buffer_info(
-                im.data,                                            // pointer to data
-                sizeof(unsigned char),                              // item size
-                py::format_descriptor<unsigned char>::format(),     // item descriptor
-                3,                                                  // matrix dimensionality
-                {                                                   // buffer dimensions
-                    im.rows, 
-                    im.cols, 
-                    im.channels()
-                },          
-                {                                                   // strides in bytes
-                    sizeof(unsigned char) * im.channels() * im.cols, // (issue with padding)
-                    sizeof(unsigned char) * im.channels(),
-                    sizeof(unsigned char)
-                }
-            );
-        })
-    ;
+        py::class_<cv::Mat>(m, "Mat", py::buffer_protocol()) 
+            .def_buffer([](cv::Mat &im) -> py::buffer_info {
+                    return py::buffer_info(
+                        im.data,                                            // pointer to data
+                        sizeof(unsigned char),                              // item size
+                        py::format_descriptor<unsigned char>::format(),     // item descriptor
+                        3,                                                  // matrix dimensionality
+                        {                                                   // buffer dimensions
+                            im.rows, 
+                            im.cols, 
+                            im.channels()
+                        },          
+                        {                                                    // strides in bytes
+                            sizeof(unsigned char) * im.channels() * im.cols, // (issue with padding)
+                            sizeof(unsigned char) * im.channels(),
+                            sizeof(unsigned char)
+                        }
+                    );
+                })
+            ;
+    }
 }
-
