23cd3fd5fb7d951f50ff9af36524fd09d07d1d6b
Andreas Kuhn <andreas.kuhn@ost.ch>
2023-04-13 11:19:23 +0200

    [skip ci] vectorAdd as Python module

--- cpp/cppmodule.cu
+++ cpp/cppmodule.cu
@@ -2,6 +2,7 @@
 #include <opencv2/opencv.hpp>
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
+#include <chrono>
 
 namespace py = pybind11;
 
@@ -49,9 +50,12 @@ int main(){*/
 
     Mat img_u(M, N, CV_8UC3); // prepare return image
     
+    using namespace std::chrono;
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    start = std::chrono::high_resolution_clock::now();
     // --- vectorAdd test ---
-    int a[] = {1,2,3,4,5,6};
-	int b[] = {2,3,4,5,6,7};
+    int a[] = {1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12};
+	int b[] = {1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12};
 	auto NUMBER_OF_VECTORS = sizeof(a) / sizeof(int);
 	int c[NUMBER_OF_VECTORS] = {0};
 
@@ -75,12 +79,22 @@ int main(){*/
 	vectorAdd <<< GRID_SIZE, BLOCK_SIZE >>> (cudaA, cudaB, cudaC);
 
 	// copy back out of GPU
+    cudaDeviceSynchronize();
 	cudaMemcpy(c, cudaC, sizeof(c), cudaMemcpyDeviceToHost);
+    end = std::chrono::high_resolution_clock::now();
 
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+ 
+    std::cout << "finished computation at " << std::ctime(&end_time)
+              << "elapsed time: " << elapsed_seconds.count() << "s\n";
+
+    // print computation result
 	for (int i = 0; i < NUMBER_OF_VECTORS; i++) {
 		std::cout << c[i] << " ";
     }
     std::cout << std::endl;
+
 
     return img_d;//img_u;
 }       
