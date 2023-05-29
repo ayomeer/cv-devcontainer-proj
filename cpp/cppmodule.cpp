d9d8d6ff7c5ecfac64314f82c360f4b64c0505a6
DESKTOP-0SRFR83\ayome <andreas.kuhn@bluewin.ch>
2023-05-29 22:33:30 +0200

    Updated cppmodule.cpp

--- cpp/cppmodule.cpp
+++ cpp/cppmodule.cpp
@@ -20,27 +20,27 @@ namespace py = pybind11;
 
 // Python bindings
 PYBIND11_MODULE(cppmodule, m){
-        m.doc() = "Cpp Module for fast algorithm implementations callable from Python";
+	m.doc() = "Cpp Module for fast algorithm implementations callable from Python";
 
-				// Binding for cv::Mat class so these object can be passed back as return value
-        py::class_<cv::Mat>(m, "Mat", py::buffer_protocol()) 
-            .def_buffer([](cv::Mat &im) -> py::buffer_info {
-                    return py::buffer_info(
-                        im.data,                                            // pointer to data
-                        sizeof(unsigned char),                              // item size
-                        py::format_descriptor<unsigned char>::format(),     // item descriptor
-                        3,                                                  // matrix dimensionality
-                        {                                                   // buffer dimensions
-                            im.rows, 
-                            im.cols, 
-                            im.channels()
-                        },          
-                        {                                                    // strides in bytes
-                            sizeof(unsigned char) * im.channels() * im.cols, // (issue with padding)
-                            sizeof(unsigned char) * im.channels(),
-                            sizeof(unsigned char)
-                        }
-                    );
-                })
-            ;
-    }
+	// Binding for cv::Mat class so these object can be passed back as return value
+	py::class_<cv::Mat>(m, "Mat", py::buffer_protocol()) 
+			.def_buffer([](cv::Mat &im) -> py::buffer_info {
+							return py::buffer_info(
+									im.data,                                            // pointer to data
+									sizeof(unsigned char),                              // item size
+									py::format_descriptor<unsigned char>::format(),     // item descriptor
+									3,                                                  // matrix dimensionality
+									{                                                   // buffer dimensions
+											im.rows, 
+											im.cols, 
+											im.channels()
+									},          
+									{                                                    // strides in bytes
+											sizeof(unsigned char) * im.channels() * im.cols, // (issue with padding)
+											sizeof(unsigned char) * im.channels(),
+											sizeof(unsigned char)
+									}
+							);
+					})
+			;
+}
