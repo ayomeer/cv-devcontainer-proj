e2af614c38396b7b2f1ae87a68a1886d6624a76f
DESKTOP-0SRFR83\ayome <andreas.kuhn@bluewin.ch>
2023-06-02 05:40:05 +0200

    added unit test for checking cv2 installation

---
+++ python/unitTests.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestSum(unittest.TestCase):
+
+    def test_cv_install(self):
+        try:
+            import cv2
+        except ImportError:
+            self.fail("Failed to import cv2. Check OpenCV installation.")
+
+if __name__ == '__main__':
+    unittest.main()
